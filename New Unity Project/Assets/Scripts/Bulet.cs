543e522e8e3d8ff24dc93b2266f851838966b317
yastretel <yas.tretel@gmail.com>
2021-02-13 00:00:13 +0300

    Gun

---
+++ New Unity Project/Assets/Scripts/Bulet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Bulet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       Destroy(gameObject, t:5) ;
+    }
+
+    
+}
---
+++ New Unity Project/Assets/Scripts/Gun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject Bullet;
+    public Transform ShootPoint;
+    void Start()
+    {
+        
+    }
+
+       void Update()
+    {
+      if (Input.GetMouseButton(0))
+    {
+        Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+    }  
+    }
+}
